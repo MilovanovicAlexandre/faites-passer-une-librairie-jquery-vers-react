99803e9020cd338c5c6372ad50335d98beeccea9
Alexandre Milovanovic <alexandre.milovanovic@gmail.com>
2023-06-13 17:55:16 +0200

    Ajout du plugin de la modal en react

--- src/Pages/Error/error.jsx
+++ src/Pages/Error/error.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import './error.css'
 
 function Error() {
     return (
         <main>
-           
+            <div className='containerTextErrorAndLink'>
+              <div className='containerTextError404'>
+                <h1>HRnet</h1>
+                <p className='textError404'>Error 404</p>
+                <p className='textError404Continuation'>Assets not found</p>
+              </div>
+              <div className='containerLinkError404'>
+                <Link to='/CreateEmployee'><p className='textLinkError404'>Return to page Create Employee</p></Link>
+              </div>
+            </div>
         </main>
     )
 }
